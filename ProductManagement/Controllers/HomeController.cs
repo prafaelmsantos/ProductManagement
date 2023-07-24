5a465c697959ce2ea4f96a0d2a579404bcf0b0c7
Pedro Rafael Santos <psantos@eneida.io>
2023-07-24 19:19:51 +0100

    df

--- ProductManagement/Controllers/HomeController.cs
+++ ProductManagement/Controllers/HomeController.cs
@@ -1,11 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using ProductManagement.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProductManagement.Controllers
 {
